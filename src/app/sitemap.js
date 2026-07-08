fea61b1e7ce0effaa37c929bd32efe6d4cb5bdc6
Tiaa21 <132942390+Tiaa21@users.noreply.github.com>
2026-07-09 00:50:53 +0700

    SEO

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://86works.vercel.app',
+      lastModified: new Date(),
+      priority: 1,
+    }
+
+  ]
+}
---
+++ src/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://86works.vercel.app/sitemap.xml',
+  }
+}
